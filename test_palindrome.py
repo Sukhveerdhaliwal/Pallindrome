9ec77b9f2efb599d2443d1c4431504e04736fa88
Sukhveer Dhaliwal <kaursukhveer21@gmail.com>
2026-02-16 14:31:49 -0500

    Step 5 - No Match
    if there is mismatch, returns False

--- test_palindrome.py
+++ test_palindrome.py
@@ -23,3 +23,6 @@ def test_double_letter_returns_True():
     '''Step 4: Assert that same character input Returns True'''
     assert is_palindrome("bb") is True
 
+def test_nomatch_returns_False():
+    assert is_palindrome("abc") is False
+
