32ca518b5e111d7e89142a93a27ee63dc2bf2a7c
Sukhveer Dhaliwal <kaursukhveer21@gmail.com>
2026-02-16 14:16:22 -0500

    Step 1 - Non-String Input
    Raises ValueError if non-String input.

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,9 @@
 """
 Validates strings as palindromes.
 """
+from collections import deque
+
+def is_palindrome(Input):
+    if not isinstance(Input, str):
+        raise ValueError('Input must be a string')
+    return False
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,14 @@
 """
 Tests the palindrome module
 """
+import pytest
+
+from palindrome import is_palindrome
+
+def test_non_string_input_Raises_ValueErro():
+    with pytest.raises(ValueError):
+        is_palindrome(988)
+    with pytest.raises(ValueError):
+        is_palindrome([1,2,2])
+    with pytest.raises(ValueError):
+        is_palindrome(3.4)
